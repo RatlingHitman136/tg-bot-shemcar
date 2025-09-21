f080829590ae83ad0eaef52d2b709de37723074f
Borys Trehubenko <borys.trehubenko@gmail.com>
2025-09-21 10:49:09 +0200

    added exec.sh file

---
+++ exec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONTAINER_NAME="tg-bot-shemcar"
+
+# Stop and remove the container if it exists
+if [ "$(docker ps -aq -f name=^${CONTAINER_NAME}$)" ]; then
+    echo "Stopping and removing existing container: $CONTAINER_NAME"
+    docker stop "$CONTAINER_NAME"
+    docker rm "$CONTAINER_NAME"
+fi
+
+# Build the Docker image
+echo "Building Docker image: $CONTAINER_NAME"
+docker build -t "$CONTAINER_NAME" .
+
+# Run the Docker container
+echo "Running Docker container: $CONTAINER_NAME"
+docker run --name "$CONTAINER_NAME" "$CONTAINER_NAME"
